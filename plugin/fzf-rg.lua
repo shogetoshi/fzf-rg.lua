67549c831c670efc729d1619741a94fa3089a583
shogetoshi <188074504+shogetoshi@users.noreply.github.com>
2025-12-18 09:08:25 +0900

    動くように修正

--- plugin/fzf-rg.lua
+++ plugin/fzf-rg.lua
@@ -1,8 +1,8 @@
 -- 二重読み込み防止
-if vim.g.loaded_fzf_fd then
+if vim.g.loaded_fzf_rg then
   return
 end
-vim.g.loaded_fzf_fd = true
+vim.g.loaded_fzf_rg = true
 
 -- キーマッピング設定
 vim.keymap.set('n', '<Plug>fzf-rg', function()

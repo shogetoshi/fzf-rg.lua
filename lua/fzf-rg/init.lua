feca77299625479a13c72bf3e2bfdee09b072adc
shogetoshi <188074504+shogetoshi@users.noreply.github.com>
2025-12-25 01:24:50 +0900

    gitの場合はリポジトリ全体を探索

--- lua/fzf-rg/init.lua
+++ lua/fzf-rg/init.lua
@@ -7,7 +7,17 @@ require("fzf").default_options = {
     end
 }
 
-local source = 'rg -uuu --ignore-file ~/.ignore --color always -n ^'
+local function get_search_path()
+    local search_path = vim.fn.system("git rev-parse --show-cdup 2>/dev/null"):gsub("%s+$", "")
+    if search_path == "" then
+        return ""
+    else
+        return search_path
+    end
+end
+
+local search_path = get_search_path()
+local source = 'rg -uuu --ignore-file ~/.ignore --color always -n ^ ' .. search_path
 local options = {
     "--ansi",
     "--multi",
@@ -15,7 +25,7 @@ local options = {
     "--delimiter", ":",
     "--preview 'bat --plain --number --color always --highlight-line={2} {1}'",
     "--preview-window '+{2}-5:down:70%'",
-    "--bind 'alt-h:reload:fd -HI -c always -t f'",
+    "--bind 'alt-h:reload:fd -HI -c always -t f " .. search_path .. "'",
 }
 
 M.run = function()
